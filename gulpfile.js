cbbe6c9cb5c75ec6308ad0f0179c1324b0fe6d63
evgenywulf <web2work@web2work.ru>
2019-11-24 17:40:27 +0500

    Inspect

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    image2base64 = require('image-to-base64');
 
 gulp.task('browser-sync', function() {
     browserSync({
